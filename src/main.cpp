cbaf75f416cd5c753c5659d864248fade9008f5d
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-03-15 02:32:59 +0100

    Create getBitmap function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <Windows.h>
 #include <sstream>
+#include <gdiplus.h>
+#include <Gdiplusheaders.h>
+#include <memory>
 
 
 POINT getCursorPosition()
@@ -10,7 +13,13 @@ POINT getCursorPosition()
 	return cursorPos;
 }
 
+std::shared_ptr<Gdiplus::Bitmap> getBitmap(const std::string& filename)
+{
+	std::wstring wfilename = std::wstring(filename.begin(), filename.end());
+	auto bitmap = std::make_shared<Gdiplus::Bitmap>(wfilename);
+	return bitmap;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-
 }
