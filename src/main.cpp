7048d892bbbb3a6df2d03d294faad96dd935bcdf
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-03-15 02:03:04 +0100

    Create getCursorPosition function

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,20 @@
 #include <Windows.h>
 #include <sstream>
 
+
+POINT getCursorPosition()
+{
+	POINT cursorPos;
+	GetCursorPos(&cursorPos);
+	return cursorPos;
+}
+
 int main()
 {
+	POINT cursorPos;
+	while (true)
+	{
+		cursorPos = getCursorPosition();
+		std::cout << "X: " << cursorPos.x << "Y: " << cursorPos.y << std::endl;
+	}
 }
